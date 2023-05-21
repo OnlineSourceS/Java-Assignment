05e5f66b4b8b7f6a2c0f5cf5d861bd50e64a44a0
OnlineSourceS <97689711+OnlineSourceS@users.noreply.github.com>
2023-05-21 21:29:27 +0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import components.BasicCalculator;
+
+public class Main extends BasicCalculator {
+    public static void main(String[] args) {
+        (new Main()).getInput().calculate().getResult();
+    }
+}
