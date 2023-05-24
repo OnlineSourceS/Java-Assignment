c9a9b3c3b9ad6db93e86916af72bbc1b34bdf857
OnlineSourceS <97689711+OnlineSourceS@users.noreply.github.com>
2023-05-24 17:21:22 +0500

    Delete src directory

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-import components.BasicCalculator;
-
-public class Main extends BasicCalculator {
-    public static void main(String[] args) {
-        (new Main()).getInput().calculate().getResult();
-    }
-}
